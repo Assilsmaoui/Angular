3f35c609fda138adc78c563e528ca8b05c060f65
Assilsmaoui <assilsmaoui71@gmail.com>
2025-03-01 08:41:32 +0100

    fourd  commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MemberComponent } from './member/member.component';
 import { MemberFormComponent } from './member-form/member-form.component';
+import { DashbordComponent } from './dashbord/dashbord.component';
+import { ToolComponent } from './tool/tool.component';
+import { EventComponent } from './event/event.component';
+import { ArticleComponent } from './article/article.component';
 
 const routes: Routes = [
   {
@@ -14,9 +18,36 @@ pathMatch:'full',
     component:MemberFormComponent
   },
   {
+    path:'member',
+    pathMatch:'full',
+    component:MemberComponent
+  },
+  {
     path:'edit/:id',
     pathMatch:'full',
     component:MemberFormComponent
+  },
+  {
+    
+      path:'dashboard',
+      pathMatch:'full',
+      component:DashbordComponent
+    
+  },
+  {
+    path:'tools',
+    pathMatch:'full',
+    component:ToolComponent
+  },
+  {
+    path:'events',
+    pathMatch:'full',
+    component:EventComponent
+  },
+  {
+    path:'articles',
+    pathMatch:'full',
+    component:ArticleComponent
   },
   {
     path:'**',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,13 +13,27 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ConfirmDialogComponent } from './confirm-dialog/confirm-dialog.component';
 import {MatDialogModule} from '@angular/material/dialog';
+import { TemplateComponent } from './template/template.component';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatListModule} from '@angular/material/list';
+import {MatMenuModule} from '@angular/material/menu';
+import { DashbordComponent } from './dashbord/dashbord.component';
+import { ToolComponent } from './tool/tool.component';
+import { EventComponent } from './event/event.component';
+import { ArticleComponent } from './article/article.component'; 
 
 @NgModule({
   declarations: [
     AppComponent,
     MemberComponent,
     MemberFormComponent,
-    ConfirmDialogComponent
+    ConfirmDialogComponent,
+    TemplateComponent,
+    DashbordComponent,
+    ToolComponent,
+    EventComponent,
+    ArticleComponent
   ],
   imports: [
     BrowserModule,
@@ -27,7 +41,7 @@ import {MatDialogModule} from '@angular/material/dialog';
     BrowserAnimationsModule,
     MatTableModule,
     MatIconModule,HttpClientModule,FormsModule,ReactiveFormsModule,
-    MatFormFieldModule,MatDialogModule
+    MatFormFieldModule,MatDialogModule,MatSidenavModule,MatToolbarModule,MatListModule,MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
